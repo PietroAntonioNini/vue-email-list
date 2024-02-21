f56b046799e46c6bc8023b95bd7cf2a29a23d95c
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-21 15:42:33 +0100

    email create

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return {
+            emails: []
+        }
+    },
+    mounted() {
+        
+        for(let i = 0; i < 10; i++) {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+
+                this.emails.push(result.data.response);
+
+                console.log(result);
+
+            });
+
+        }
+        
+    },
+    methods: {
+        
+    },
+
+}).mount('#app');
